c19c0e4352817a0e50c6c510a05c9ef6595bcdb5
JaredWightman <168222832+JaredWightman@users.noreply.github.com>
2024-06-07 19:02:35 -0600

    Set up simultaneous server and client

--- PeerToPeer/PeerToPeer.py
+++ PeerToPeer/PeerToPeer.py
@@ -1,69 +1,41 @@
 # Jared Wightman
 
-# DESKTOP IPv4: 10.105.178.207
-# LAPTOP IPv4:  10.105.201.165
-# PORT:         60001
-
-
+from pickle import TRUE
 import socket as s
+import threading
+
 
 HOST = s.gethostbyname(s.gethostname())
+FOREIGN = open("C:/Users/JWigh/source/repos/Local Networking IP.txt", "r").readline()
 PORT = 60001
 
-FOREIGN = ""
+def server():
+    
+    serverNode = s.socket(s.AF_INET, s.SOCK_STREAM)
+    serverNode.bind((HOST, PORT))
+    serverNode.listen()
 
-if HOST == "10.105.178.207":
-    FOREIGN = "10.105.201.165"
-elif HOST == "10.105.201.165":
-    FOREIGN = "10.105.178.207"
-
-
-node = s.socket(s.AF_INET, s.SOCK_STREAM)
-node.bind((HOST, PORT))
-
-node.listen()
-node.connect((FOREIGN, PORT))
-remote, remoteAddress = node.accept()
-
-while True: # Need to thread or sumn
-    toSend = input("> ")
-    node.sendall(toSend)
-    received = remote.recv(1024)
-    print("< ", received.decode())
-
-# while 1:
-
-#     command = input("> ")
-#     match command.upper():
-#         case "EXIT":
-#             break
-#         case "CONNECT":
-#             foreignhost = input("CONNECT TO IP: ")
-#             print("Attempting to connect to '{}'".format(foreignhost)) 
-#             # Add loading dots here...threading?
-#             node.connect((foreignhost, PORT))
-#             # Raise error
+    while True:
+        remote, remote_address = serverNode.accept()
+        while True:
+            received = remote.recv(1024)
+            print("          ", received.decode())
             
-#             node.sendall(b'Hello, server')
-#             # Receive data from the server
-#             data = node.recv(1024)
-
-#             print('Received:', data.decode())
-#         case "LISTEN":
-#             print("Listening...")
-#             node.listen()
-#             # Accept a connection
-#             remoteNode, addr = node.accept() # conn is the other socket!
-
-#             print('Connected by', addr)
-#             while True:
-#                 # Receive data from the client
-#                 data = remoteNode.recv(1024)
-#                 if not data:
-#                     break
-#                 print('Received:', data.decode())
-#                 # Send a response back to the client
-#                 remoteNode.sendall(data)
+def client():
+    
+    clientNode = s.socket(s.AF_INET, s.SOCK_STREAM)
+    clientNode.connect((FOREIGN, PORT))
+    
+    while True:
+        to_send = input("> ")
+        clientNode.sendall(to_send.encode())
 
 
-#     print("Cycled 'While' loop")
+serverThread = threading.Thread(target=server)        
+clientThread = threading.Thread(target=client)
+
+serverThread.start()
+clientThread.start()
+
+
+
